ab2c9aa851ea764cd7012e9e8bc49bb7b78a1256
Kellyane da Silva Souza <kellyane.s@academico.ifrn.edu.br>
2023-04-10 17:27:25 -0300

    10/04

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class LoadScenebutton : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
 
     [SerializeField] private PlayerInput _playerInput;
